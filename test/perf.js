b6d41279e6bf55de3b11fdf1ffb7bd7a6ff701f9
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2013-12-30 00:03:34 +0000

    clean up

--- test/perf.js
+++ test/perf.js
@@ -6,14 +6,14 @@ module.exports = perfTest;
 
 function perfTest(func1, func2, times) {
 	// remove return to run performance tests
-	return;
+	// return;
 
 	times = times || 100000;
 	var time1 = time(func1);
 	var time2 = time(func2);
 
 	// change cnsl to console and uncomment to run test
-	// cnsl.log('time1:', time1, 'time2:', time2, 'diff: func1 is ', time1 / time2, 'times slower (if < 1 - faster)');
+	console.log('time1:', time1, 'time2:', time2, 'diff: func1 is ', time1 / time2, 'times slower (if < 1 - faster)');
 
 	function time(func) {
 		var start = Date.now()
