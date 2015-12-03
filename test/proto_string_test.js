6bd9d4d862ab6d2ad8f9f100d86c459b84e60bbb
Jason Green <jasoniangreen@gmail.com>
2015-12-03 17:56:05 +0000

    [Jason, Evgeny] string functions, one left

--- test/proto_string_test.js
+++ test/proto_string_test.js
@@ -1,16 +1,25 @@
 'use strict';
 
-var _ = require('../lib/proto')
+var _ = require('../lib/proto2')
     , assert = require('assert');
 
 
-describe('String functions', function() {
+describe.only('String functions', function() {
     it('should define firstUpperCase function', function() {
         var upper = 'UPPERCASE'
             , lower = 'lowercase';
 
         assert.equal(_.firstUpperCase(upper), 'UPPERCASE');
         assert.equal(_.firstUpperCase(lower), 'Lowercase');
+    });
+
+
+    it('should define firstUpperCase method', function() {
+        var upper = 'UPPERCASE'
+            , lower = 'lowercase';
+
+        assert.equal(_(upper).firstUpperCase()._(), 'UPPERCASE');
+        assert.equal(_(lower).firstUpperCase()._(), 'Lowercase');
     });
 
 
@@ -23,6 +32,15 @@ describe('String functions', function() {
     });
 
 
+    it('should define firstLowerCase method', function() {
+        var upper = 'UPPERCASE'
+            , lower = 'lowercase';
+
+        assert.equal(_(upper).firstLowerCase()._(), 'uPPERCASE');
+        assert.equal(_(lower).firstLowerCase()._(), 'lowercase');
+    });
+
+
     it('should define toRegExp function', function() {
         var pattern = /ab+c/i
             , patternStr = pattern.toString();
@@ -30,6 +48,19 @@ describe('String functions', function() {
         assert.equal(patternStr, '/ab+c/i');
 
         var regex = _.toRegExp(patternStr);
+        assert.equal(patternStr, regex.toString());
+        assert(regex instanceof RegExp);
+        assert(regex.test('ABBC'));
+    });
+
+
+    it('should define toRegExp method', function() {
+        var pattern = /ab+c/i
+            , patternStr = pattern.toString();
+
+        assert.equal(patternStr, '/ab+c/i');
+
+        var regex = _(patternStr).toRegExp()._();
         assert.equal(patternStr, regex.toString());
         assert(regex instanceof RegExp);
         assert(regex.test('ABBC'));
@@ -49,6 +80,19 @@ describe('String functions', function() {
     });
 
 
+    it('should define toFunction method', function() {
+        function myFunc() { return 1234; }
+        var funcStr = myFunc.toString();
+
+        assert.equal(funcStr, 'function myFunc() { return 1234; }');
+
+        var func = _(funcStr).toFunction()._();
+        assert.equal(funcStr, func.toString());
+        assert.equal(typeof func, 'function');
+        assert.equal(func(), 1234);
+    });
+
+
     it('should define toQueryString function', function() {
         var params = {name: 'Jason', age: 30};
 
@@ -57,10 +101,26 @@ describe('String functions', function() {
     });
 
 
+    it('should define toQueryString method', function() {
+        var params = {name: 'Jason', age: 30};
+
+        var str = _(params).toQueryString()._();
+        assert.equal(str, 'name=Jason&age=30');
+    });
+
+
     it('should define fromQueryString function', function() {
         var str = 'name=Jason&age=30'; ;
 
         var params = _.fromQueryString(str);
+        assert.deepEqual(params, {name: 'Jason', age: 30});
+    });
+
+
+    it('should define fromQueryString method', function() {
+        var str = 'name=Jason&age=30'; ;
+
+        var params = _(str).fromQueryString()._();
         assert.deepEqual(params, {name: 'Jason', age: 30});
     });
 
@@ -75,7 +135,17 @@ describe('String functions', function() {
     });
 
 
-    it('should define jsonParse', function() {
+    it('should define toDate method', function() {
+        assert.equal(_(null).toDate()._(), undefined);
+        assert.equal(_(undefined).toDate()._(), undefined);
+        assert.equal(_('').toDate()._(), undefined);
+        assert.equal(_('abc').toDate()._(), undefined);
+        assert.equal(_('2014-19-02').toDate()._(), undefined);
+        assert.equal(_('2014-02-19').toDate()._().toString(), new Date('2014-02-19').toString());
+    });
+
+
+    it('should define jsonParse function', function() {
         var json = '{"test":1, "name":"milo"}'
             , badJson = '{"test:1';
 
@@ -87,10 +157,33 @@ describe('String functions', function() {
     });
 
 
+    it('should define jsonParse method', function() {
+        var json = '{"test":1, "name":"milo"}'
+            , badJson = '{"test:1';
+
+        assert.deepEqual(_(json).jsonParse()._(), { test: 1, name: 'milo' });
+        assert.equal(_(badJson).jsonParse()._(), undefined);
+        assert.throws(function() {
+            JSON.parse(badJson);
+        });        
+    });
+
+
     it('should define hashCode function', function() {
         var result1 = _.hashCode('This was no small decision. Four generations of Orr men had been Eagles, including Ron and Andrew\'s older brother. Andrew had spent years working toward Scouting\'s highest rank, and was just months from reaching it.');
         assert.equal(typeof result1, 'number');
         var result2 = _.hashCode('But the Boy Scouts had decided to admit gays, and Ron Orr, a tall, soft-spoken man with a firm handshake, is clear about his Christian faith and what it says about homosexuality: It is a sin that cannot be tolerated.');
+        assert.equal(typeof result2, 'number');
+        assert(result1 != result2);
+    });
+
+
+    it('should define hashCode method', function() {
+        var result1 = _('This was no small decision. Four generations of Orr men had been Eagles, including Ron and Andrew\'s older brother. Andrew had spent years working toward Scouting\'s highest rank, and was just months from reaching it.')
+                        .hashCode()._();
+        assert.equal(typeof result1, 'number');
+        var result2 = _('But the Boy Scouts had decided to admit gays, and Ron Orr, a tall, soft-spoken man with a firm handshake, is clear about his Christian faith and what it says about homosexuality: It is a sin that cannot be tolerated.')
+                        .hashCode()._();
         assert.equal(typeof result2, 'number');
         assert(result1 != result2);
     });
